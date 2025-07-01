690699aafa516b70d7f4da7237875ed9fbe9bd2d
Ranjit <r.singhnagi@everdreamsoft.com>
2025-07-01 20:37:33 +0200

    udpate transfer tst

--- test/inscribe2.ts
+++ test/inscribe2.ts
@@ -21,12 +21,13 @@ import * as tools from 'uint8array-tools';
 import { sha256 } from '@noble/hashes/sha256';
 import { randomBytes } from 'crypto';
 import * as readline from 'readline';
+import ECPairFactory, { ECPairAPI } from 'ecpair';
 
 bitcoin.initEccLib(ecc);
 const bip32 = BIP32Factory(ecc);
 const rng = (size: number) => randomBytes(size);
 const network = bitcoin.networks.regtest
-
+const ECPair: ECPairAPI = ECPairFactory(ecc);
 
 async function createInscriptionTx() {
     // 1. Create keys
@@ -331,12 +332,18 @@ async function createInscriptionTx2() {
 
 async function createInscriptionTx3() {
 
-    const BIP32 = BIP32Factory(ecc);
+    const wif = "cTgU4tVGog3cmC6xU2xG6BwMJntUWJj9Bt8GdxKaXDSxTTAVS5yE";
+    const keypair = ECPair.fromWIF(wif, network);
+    const pubkey = toXOnly(Buffer.from(keypair.publicKey));
 
-    const internalKey = BIP32.fromSeed(randomBytes(64), network);
-    const leafKey: BIP32Interface = BIP32.fromSeed(randomBytes(64), network);
 
-    const pubkey = toXOnly(Buffer.from(leafKey.publicKey));
+    // const { address: sendAddress } = bitcoin.payments.p2tr({
+    //     internalPubkey: pubkey,
+    //     network,
+    // });
+
+    // console.log(sendAddress);
+
 
     const leafScript = bitcoin.script.compile([
         pubkey,
@@ -361,7 +368,7 @@ async function createInscriptionTx3() {
     };
 
     const { output, witness, address } = bitcoin.payments.p2tr({
-        internalPubkey: toXOnly(Buffer.from(internalKey.publicKey)),
+        internalPubkey: pubkey,
         scriptTree,
         redeem,
         network,
@@ -423,7 +430,7 @@ async function createInscriptionTx3() {
             // 7. Add output sending all funds minus fee to another P2TR (or any address)
             // Let's just send back to internalKey P2TR address without scripts for simplicity
             const { address: sendAddress } = bitcoin.payments.p2tr({
-                internalPubkey: toXOnly(Buffer.from(internalKey.publicKey)),
+                internalPubkey: pubkey,
                 network,
             });
 
@@ -441,10 +448,10 @@ async function createInscriptionTx3() {
             // };
 
             const leafSigner: bitcoin.Signer = {
-                publicKey: Buffer.from(leafKey.publicKey),
-                sign: (hash: Buffer) => Buffer.from(leafKey.sign(hash)),
-                signSchnorr: leafKey.signSchnorr
-                    ? (hash: Buffer) => Buffer.from(leafKey.signSchnorr!(hash))
+                publicKey: pubkey,
+                sign: (hash: Buffer) => Buffer.from(keypair.sign(hash)),
+                signSchnorr: keypair.signSchnorr
+                    ? (hash: Buffer) => Buffer.from(keypair.signSchnorr!(hash))
                     : undefined,
             };
 
@@ -476,5 +483,70 @@ async function createInscriptionTx3() {
 
 }
 
-createInscriptionTx2();
+async function transferTaprootUtxo() {
+    // Your key WIF and get keypair + pubkey
+    const wif = "cTgU4tVGog3cmC6xU2xG6BwMJntUWJj9Bt8GdxKaXDSxTTAVS5yE";
+    const keypair = ECPair.fromWIF(wif, network);
+    const pubkey = toXOnly(Buffer.from(keypair.publicKey));
+
+    console.log(pubkey.toString('hex'))
+
+    try {
+
+        const utxo = {
+            txid: "a356110f922356ec9f191de4e30d5cde4659ea5aa3a04cfcf4b4d0daa10ad28c",
+            value: 9000,
+            vout: 0,
+            hex: "512062497f69e6b63aa4b057db330433863a2f829cd976b2aaf845893fc9cc5f34e7"
+        }
+
+        // For simplicity, send back to the same P2TR address (can replace here)
+        const { address: sendAddress , output} = bitcoin.payments.p2tr({ internalPubkey: pubkey, network })
+
+        const fee = 1000
+        const sendAmount = utxo.value - fee
+
+        const psbt = new bitcoin.Psbt({ network })
+
+        // Taproot key path spend (no scriptTree, simple keyspend)
+        psbt.addInput({
+            hash: utxo.txid,
+            index: utxo.vout,
+            witnessUtxo: {
+                script: output!,
+                value: utxo.value,
+            },
+            tapInternalKey: pubkey,
+        })
+
+        psbt.addOutput({
+            address: sendAddress!,
+            value: sendAmount,
+        })
+
+        // Sign input with keypair (key spend)
+        const leafSigner: bitcoin.Signer = {
+            publicKey: pubkey,
+            sign: (hash: Buffer) => Buffer.from(keypair.sign(hash)),
+            signSchnorr: keypair.signSchnorr
+                ? (hash: Buffer) => Buffer.from(keypair.signSchnorr!(hash))
+                : undefined,
+        };
+
+        psbt.signInput(0, leafSigner);
+
+        psbt.finalizeAllInputs()
+
+        const tx = psbt.extractTransaction()
+        const txHex = tx.toHex()
+
+        console.log('Raw transaction hex:\n', txHex)
+    } catch (e) {
+        console.error('Error:', e)
+    } finally {
+
+    }
+}
+
+transferTaprootUtxo();
 
