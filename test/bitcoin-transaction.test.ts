dad28d3f2d182f0a76ac31696ae0566054a62463
Ranjit <r.singhnagi@everdreamsoft.com>
2025-07-03 16:48:15 +0200

    updated test bitcoin tx

--- test/bitcoin-transaction.test.ts
+++ test/bitcoin-transaction.test.ts
@@ -1,12 +1,10 @@
 import { expect } from 'chai';
 import { BitcoinAddress } from '../src/wallets/bitcoin/address.js';
 import { BitcoinTransactionParams, NetworkType } from '../src/types/common.js';
-import { BitcoinWallet } from '../src/wallets/bitcoin/wallet.js';
 import { BitcoinRpcProvider } from '../src/providers/bitcoin/bitcoin-rpc.js';
 import { BitcoinTransaction } from '../src/providers/bitcoin/utils/bitcoin-transaction.js';
-import { error } from 'console';
 
-const network: NetworkType = "regtest"
+const network: NetworkType = "regtest";
 const rpc = new BitcoinRpcProvider({
     url: 'http://127.0.0.1:18443',
     username: 'ranjit',
@@ -16,14 +14,17 @@ const rpc = new BitcoinRpcProvider({
 describe('BitcoinTransaction - regtest', function () {
     this.timeout(50000);
 
-    const addressObject = new BitcoinAddress({ wif: "cNR3Ghixdw4QYfY4ZULKBF51kxVtD6EBCDTxBkHmunDTGCwnz8J8", network })
-    let walletName = "test-rpc";
-    let address = addressObject.getAddress('p2wpkh')
-    let descriptor = addressObject.getDescriptor('p2wpkh')
+    const addressObject = new BitcoinAddress({
+        wif: "cNR3Ghixdw4QYfY4ZULKBF51kxVtD6EBCDTxBkHmunDTGCwnz8J8",
+        network
+    });
+
+    const walletName = "test-rpc";
+    const fromAddress = addressObject.getAddress('p2wpkh');
+    const fromDescriptor = addressObject.getDescriptor('p2wpkh');
     let toAddress = "";
 
     before(async () => {
-        // Checking wallet, importing address to wallet 
         try {
             await rpc.createWallet(walletName);
         } catch (e: any) {
@@ -36,30 +37,26 @@ describe('BitcoinTransaction - regtest', function () {
             if (!e.message.includes('already loaded')) throw e;
         }
 
-        const { checksum } = await rpc.getDescriptorInfo(descriptor);
-        const descWithChecksum = `${descriptor}#${checksum}`
+        const { checksum } = await rpc.getDescriptorInfo(fromDescriptor);
+        const descWithChecksum = `${fromDescriptor}#${checksum}`;
         await rpc.importAddressToWallet(walletName, descWithChecksum, "test-address");
 
-        toAddress = await rpc.getNewAddress(walletName, "test-ptr", 'bech32m') // taprrot??
+        toAddress = await rpc.getNewAddress(walletName, "test-to", 'bech32m');
+        expect(toAddress).to.be.a('string').that.includes('bcrt'); // Regtest address check
 
-        // Funding to have utxos and balacnce 
-        await rpc.generateToAddress(101, address)
-        await rpc.generateToAddress(101, toAddress)
+        // Fund both addresses
+        await rpc.generateToAddress(101, fromAddress);
+        await rpc.generateToAddress(101, toAddress);
 
-        console.log(toAddress)
-
+        const fromBalance = await rpc.getBalanceAddress(walletName, fromAddress);
+        const toBalance = await rpc.getBalanceAddress(walletName, toAddress);
+        expect(fromBalance).to.be.above(0);
+        expect(toBalance).to.be.above(0);
     });
 
-
-    it('should create a valid regtest transaction', async () => {
-
-        console.log(address);
-        let bal = await rpc.getBalanceAddress(walletName, address);
-        let utxos = await rpc.listUnspentAddress(walletName, address);
-
-        //console.log(utxos);
-
-        console.log(await rpc.getBalanceAddress(walletName, toAddress))
+    it('should create and send a valid regtest transaction', async () => {
+        const utxos = await rpc.listUnspentAddress(walletName, fromAddress);
+        expect(utxos).to.be.an('array').that.is.not.empty;
 
         const { hex, fee, outputs } = await BitcoinTransaction.create({
             amountSats: 100000,
@@ -67,19 +64,17 @@ describe('BitcoinTransaction - regtest', function () {
             from: addressObject,
             utxos,
             feeRate: 1
-        }, network)
+        } as BitcoinTransactionParams, network);
 
-        const a = await rpc.sendRawTransaction(hex);
+        expect(hex).to.be.a('string').that.matches(/^[0-9a-f]+$/i);
+        expect(fee).to.be.a('number').that.is.above(0);
 
-        console.log(a, hex)
+        const txid = await rpc.sendRawTransaction(hex);
+        expect(txid).to.be.a('string').that.has.lengthOf(64);
 
+        await rpc.generateToAddress(1, fromAddress);
 
-        await rpc.generateToAddress(1, addressObject.getAddress('p2wpkh'))
-
-        console.log(await rpc.getBalanceAddress(walletName, toAddress))
-
-
+        const finalBalance = await rpc.getBalanceAddress(walletName, toAddress);
+        expect(finalBalance).to.be.greaterThan(0.001); // Should be > 0.001 BTC (100000 sats)
     });
-
-
 });
